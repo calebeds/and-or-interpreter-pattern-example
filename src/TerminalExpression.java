1167ccf396cc6ea365d372be8a0610a061873d38
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-06-05 22:07:43 -0300

    add interpret pattern example

---
+++ src/TerminalExpression.java
@@ -0,0 +1,12 @@
+public class TerminalExpression implements Expression {
+   private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(Context context) {
+        return context.getResult(data);
+    }
+}
